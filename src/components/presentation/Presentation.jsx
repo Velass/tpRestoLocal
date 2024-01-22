dd1a0553accb308eb476c8508f70cba4367de005
florian <furnari.florian@hotmail.fr>
2024-01-22 20:19:49 +0100

    test du run build tout est ok pour moi test coter git

--- src/components/presentation/Presentation.jsx
+++ src/components/presentation/Presentation.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/no-unescaped-entities */
-import "./presentation.css";
+import "./Presentation.css";
 function Presentation() {
   return (
     <div className="presentation">
